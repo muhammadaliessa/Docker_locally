d53c601962b6b1ed1e2c3458074d1a6306a2b69e
unknown <muhammadaliessa@gmail.com>
2022-11-03 22:22:58 +0200

    version 21

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a6020d21de7794c8ead2d3f389890861-2001378266.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
